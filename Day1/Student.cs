87aef334abdc6e393b1304344926d2caa9ae361f
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-03-28 20:50:51 +0200

    Initial commit

---
+++ Day1/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day1
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student()
+        {
+            ;
+        }
+
+        public Student(int id, string name, int age) 
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+        }
+    }
+}
